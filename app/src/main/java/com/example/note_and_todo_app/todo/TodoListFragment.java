a5a585fb50ad22451ae8adfe5e7d6bfbff1db97d
BUI-XUAN-HUAN <89187491+huantca@users.noreply.github.com>
2023-03-10 13:25:11 +0700

    contruct folder

--- app/src/main/java/com/example/note_and_todo_app/todo/TodoListFragment.java
+++ app/src/main/java/com/example/note_and_todo_app/todo/TodoListFragment.java
@@ -1,4 +1,4 @@
-package com.example.note_and_todo_app;
+package com.example.note_and_todo_app.todo;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.note_and_todo_app.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link fragment_setting#newInstance} factory method to
+ * Use the {@link TodoListFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class fragment_setting extends Fragment {
+public class TodoListFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class fragment_setting extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public fragment_setting() {
+    public TodoListFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class fragment_setting extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_setting.
+     * @return A new instance of fragment fragment_todo_list.
      */
     // TODO: Rename and change types and number of parameters
-    public static fragment_setting newInstance(String param1, String param2) {
-        fragment_setting fragment = new fragment_setting();
+    public static TodoListFragment newInstance(String param1, String param2) {
+        TodoListFragment fragment = new TodoListFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class fragment_setting extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        return inflater.inflate(R.layout.fragment_todo_list, container, false);
     }
 }
