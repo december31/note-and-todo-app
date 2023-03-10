239abff5b3e388e3fc12eebda784d97687d7f389
BUI-XUAN-HUAN <89187491+huantca@users.noreply.github.com>
2023-03-10 13:25:11 +0700

    contruct folder

--- app/src/main/java/com/example/note_and_todo_app/todo/TodoCategoryFragment.java
+++ app/src/main/java/com/example/note_and_todo_app/todo/TodoCategoryFragment.java
@@ -1,4 +1,4 @@
-package com.example.note_and_todo_app;
+package com.example.note_and_todo_app.todo;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.note_and_todo_app.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link fragment_todo_category#newInstance} factory method to
+ * Use the {@link TodoCategoryFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class fragment_todo_category extends Fragment {
+public class TodoCategoryFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class fragment_todo_category extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public fragment_todo_category() {
+    public TodoCategoryFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +39,8 @@ public class fragment_todo_category extends Fragment {
      * @return A new instance of fragment fragment_todo_category.
      */
     // TODO: Rename and change types and number of parameters
-    public static fragment_todo_category newInstance(String param1, String param2) {
-        fragment_todo_category fragment = new fragment_todo_category();
+    public static TodoCategoryFragment newInstance(String param1, String param2) {
+        TodoCategoryFragment fragment = new TodoCategoryFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
